820e80945b5f144c2d6bb0a7efe249c393d4cca7
Atul Varshneya <atul.varshneya@gmail.com>
2016-07-28 08:29:01 -0700

    some work on fouriertransform, and a bit of corrections in teh document.

--- qclib/examples/fourier-sampling.py
+++ qclib/examples/fourier-sampling.py
@@ -1,5 +1,35 @@
 import qclib
+import numpy as np
 
-nqbits = 8
+nqbits = 4
 
-# create the QFT operator for the given number of qbits
+# create a list of nth roots of unity
+def rootsofunity(n,step):
+	rootlist = [None]*n
+	theta = 2 * np.pi / n
+	for i in range(n):
+		pow = i * step
+		pow = pow % n
+		# e^(2*pi/n)*pow
+		c = np.cos(theta*pow)
+		s = np.sin(theta*pow)
+		rootlist[i] = complex(c,s)
+	return rootlist
+
+# ... and using that, create the QFT operator for the given number of qbits
+def qftop(n):
+	opmat = [None]*(2**n)
+	for i in range(2**n):
+		opmat[i] = rootsofunity(2**n,i)
+	opmat = np.matrix(opmat,dtype=complex) / (2**(nqbits/2))
+	oper = ["QFT",opmat]
+	return oper
+
+try:
+	qc = qclib.qcsim(4,qtrace=True)
+	for i in range(nqbits):
+		qc.qgate(qc.H(),[i])
+	qc.qgate(qftop(nqbits), range(nqbits))
+	# print qftop(nqbits)
+except qclib.QClibError,ex:
+	print ex.args
