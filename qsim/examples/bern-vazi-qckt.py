052a25390a531d94e9776c9f912f8bca4f65e904
Atul Varshneya <atul.varshneya@gmail.com>
2021-03-28 16:41:11 -0700

    bern-vazi-qckt.py is not well written, still fixed some nits.

--- qsim/examples/bern-vazi-qckt.py
+++ qsim/examples/bern-vazi-qckt.py
@@ -19,24 +19,24 @@ Problem Statement:
 ###########################################################################
 def get_fxckt(nq):
 	nqubits = nq
-	input_sz = nqubits - 1
-	secret_code = rand.randint(0,2**input_sz-1)
-	code_fmt = "{:0"+"{:d}".format(input_sz)+"b}"
+	## fx has nqubits -1 inputs, and the remaining one qubit is the output
+	secret_code = rand.randint(0,2**(nqubits-1)-1)
+	code_fmt = "{:0"+"{:d}".format(nqubits-1)+"b}"
 	print("Pssst... the secret code is ",code_fmt.format(secret_code))
 	fxckt = qckt.QCkt(nqubits)
-	for i in range(input_sz):
+	for i in range(nqubits-1):
 		if secret_code & (0x1<<i):
-			fxckt.CX(i,input_sz)
+			fxckt.CX(i,nqubits-1)
 	# fxckt.draw()
 	return fxckt
 
 class bernvazi:
 	def __init__(self, qsimsz):
-		self.inputsz = qsimsz -1
+		self.nqubits = qsimsz
 
 	def gen_bv_ckt(self):
 		print("Getting the secret function box...")
-		fxckt = get_fxckt(self.inputsz+1) # returns FX that acts on self.inputsz + 1 qbits
+		fxckt = get_fxckt(self.nqubits) # returns FX that acts on self.nqubits qbits
 		print("OK, FX is ready.")
 
 		###########################################################################
@@ -47,15 +47,15 @@ class bernvazi:
 
 		###########################################################################
 		# Step 0: Prepare the result bit |b> to |->
-		bv_ckt = qckt.QCkt(self.inputsz+1)
-		bv_ckt.X(self.inputsz)
-		bv_ckt.H(self.inputsz)
+		bv_ckt = qckt.QCkt(self.nqubits)
+		bv_ckt.X(self.nqubits-1)
+		bv_ckt.H(self.nqubits-1)
 		print("Step 0: Prepared |b> as |->")
 		# bv_ckt.draw()
 
 		###########################################################################
 		## Step 1: Apply H on all qbits of |x>
-		for i in range(self.inputsz):
+		for i in range(self.nqubits-1):
 			bv_ckt.H(i)
 		print("Step 1: Applied H to all |x> qbits")
 		# bv_ckt.draw()
@@ -63,9 +63,10 @@ class bernvazi:
 		###########################################################################
 		## Step 2: Now apply the secret function f()
 		bv_ckt.Border()
-		qbit_list = list(range(self.inputsz+1))
-		# qbit_list.reverse()
-		fxckt = fxckt.realign(self.inputsz+1,self.inputsz+1,qbit_list)
+		### not sure why this code was there at all
+		### as realign() was defined at that time, these two lines is basically a NOOP
+		### qbit_list = list(range(self.nqubits))
+		### fxckt = fxckt.realign(self.nqubits,self.nqubits,qbit_list)
 		bv_ckt = bv_ckt.append(fxckt)
 		bv_ckt.Border()
 		print("Step 2: Applied FX to |b>|x>")
@@ -73,22 +74,22 @@ class bernvazi:
 
 		###########################################################################
 		## Step 3: Again apply H on all qbits of |x>
-		for i in range(self.inputsz):
+		for i in range(self.nqubits-1):
 			bv_ckt.H(i)
 		print("Step 3: Again Applied H to all |x> qbits")
 		# bv_ckt.draw()
 
 		###########################################################################
 		## Step 4: Measure all qbits of |x>
-		bv_ckt.M(list(range(self.inputsz))) # this will give [LSB, ..., MSB]
+		bv_ckt.M(list(range(self.nqubits-1)))
 		print("Step 4: measure all |x> qbits")
-		bv_ckt.draw()
+		# bv_ckt.draw()
 
 		return bv_ckt
 
 
 if __name__ == "__main__":
-	nqubits = 6
+	nqubits = 8
 	bv = bernvazi(nqubits)
 	bv_ckt = bv.gen_bv_ckt()
 	bk = qckt.Backend()
