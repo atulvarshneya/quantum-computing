89eea44e823026aaf7fde327bb1cbfcfcab0193b
Atul Varshneya <atul.varshneya@gmail.com>
2021-03-29 18:10:19 -0700

    qsim and qckt both have examples folders now

---
+++ qsim/examples/deutsch-jozsa.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import random as rnd
+import numpy as np
+import qsim
+
+print("""
+-------------------------------------------------------------------------------------------------------
+Problem Statement:
+  Given a function whose output is CONSTANT (fixed output in the result
+  register) or BALANCED (result register is equally 0 and 1 across all
+  possible input values. Problem is to find which fx it is.
+
+This program randomly selects one of the two fx's and the algorithm
+determines which one it is.
+-------------------------------------------------------------------------------------------------------
+""")
+
+nqbits = 8
+
+def fx(q):
+	toss = int(rnd.random()*2.0)
+	if toss == 0:
+		print("fx is CONSTANT.")
+	elif toss == 1:
+		print("fx is BALANCED.")
+		q.qgate(q.C(),[1,0])
+
+try:
+	q = qsim.QSimulator(nqbits)
+	q.qgate(q.X(),[0])
+	q.qgate(q.H(),[0])
+
+	for i in range(nqbits-1):
+		q.qgate(q.H(),[i+1])
+
+	fx(q)
+	# q.qmeasure([0]) # measure it if you like, does not change anything
+
+	for i in range(nqbits-1):
+		q.qgate(q.H(),[i+1])
+
+	v = q.qmeasure(list(range(nqbits-1,0,-1)),qtrace=False)
+	if 1 in v:
+		print("Found fx is BALANCED")
+	else:
+		print("Found fx is CONSTANT")
+
+except qsim.QSimError as ex:
+	print(ex.args)
