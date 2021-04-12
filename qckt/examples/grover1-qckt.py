87a9800c2681f773b36eece1f7acf381da137a13
Atul Varshneya <atul.varshneya@gmail.com>
2021-04-11 19:54:40 -0700

    aligned the older gover algo to qckt

--- qckt/examples/grover1-qckt.py
+++ qckt/examples/grover1-qckt.py
@@ -4,39 +4,15 @@ import qckt
 import numpy as np
 import random as rnd
 
-
-def intro():
-	print("""Grover's Algorithm.
-
-Problem Statement:
-Well known search algorithm by Lov Grover.
-
-	      +----+     +------+     +----+    +------+    +----+
-n-1	|0> --|    |-----|      |-----|    |----|      |----|    |--- ... --(/)
-n-2	|0> --|    |-----|      |-----|    |----|      |----|    |--- ... --(/)
-n-3	|0> --| Hn |-----|  Uf  |-----| Hn |----| UinvM|----| Hn |--- ... --(/)
-	       ...         ...         ...        ...        ...
-2	|0> --|    |-----|      |-----|    |----|      |----|    |--- ... --(/)
-1	|0> --|    |-----|      |-----|    |----|      |----|    |--- ... --(/)
-	      +----+     |      |     +----+    |      |    +----+
-0	|-> -------------|      |---------------|      |-------------
-                     +------+               +------+
-
-	     \-Init-/  \------------------Iterate-------------------/  \-Measure-/
-""")
-
 #################################################################################
 # Details @ https://en.wikipedia.org/wiki/Grover%27s_algorithm
 #################################################################################
 
-# intro()
-
-nqbits = 5
-marker = int(rnd.random() * (2**(nqbits-1)-1))
-print(("Marker to search = {:0"+str(nqbits-1)+"b}").format(marker))
+nqbits = 7
 
 # 'needle' in the haytack = key
-print("Building Uf circuit - phase inversion of the marker ...")
+marker = int(rnd.random() * (2**(nqbits-1)-1))
+print(("Marker to search = {0:0"+str(nqbits-1)+"b}, ({0:d})").format(marker))
 uf_ckt = qckt.QCkt(nqbits,name="Uf")
 uf_ckt.Border()
 x_list = []
@@ -49,16 +25,15 @@ uf_ckt.X(x_list)
 uf_ckt.Border()
 # uf_ckt.draw()
 
-print("Buiding Amplifier circuit - phase inversion about the mean ...")
-amp_ckt = qckt.QCkt(nqbits,name="Amplifier")
+amp_ckt = qckt.QCkt(nqbits,name="Diffuser")
 amp_ckt.H([i for i in range(nqbits-1)])
 amp_ckt.X([i for i in range(nqbits-1)])
-amp_ckt.CX(*[i for i in range(nqbits)])
+amp_ckt.CX(*[i for i in range(nqbits)])   ## This is how Umesh Vazirni explains it
+# amp_ckt.CZ(*[i for i in range(nqbits-1)]) ## Gives identical results ... since still invertig phase of the inputs register.
 amp_ckt.X([i for i in range(nqbits-1)])
 amp_ckt.H([i for i in range(nqbits-1)])
 # amp_ckt.draw()
 
-print("Buiding Initializer circuit - uniform superposition ...")
 init_ckt = qckt.QCkt(nqbits,name="Initialize")
 init_ckt.H([i for i in range(nqbits-1)])
 # setup the result qubit in |-> state for phase kickback
@@ -66,22 +41,42 @@ init_ckt.X(nqbits-1)
 init_ckt.H(nqbits-1)
 # init_ckt.draw()
 
-fullckt = qckt.QCkt(nqbits)
+fullckt = qckt.QCkt(nqbits,name="Full Grover's Circuit")
 fullckt = fullckt.append(init_ckt)
-# fullckt.Probe("after initialization", probestates=[marker])
+fullckt.Probe("after initialization", probestates=[marker])
 numitrs = int((np.pi/4.0) * (2.0**((nqbits-1.0)/2.0))) # optimal # iter, less or more dont work
-print("number of Invert-Amplify iterations = ",numitrs)
+print("number of Invert-Diffuser iterations = ",numitrs)
 for itr in range(numitrs):
 	fullckt = fullckt.append(uf_ckt)
 	fullckt = fullckt.append(amp_ckt)
-	# fullckt.Probe("after iteration "+str(itr+1), probestates=[marker])
+	fullckt.Probe("after iteration "+str(itr+1), probestates=[marker])
 fullckt.M([i for i in range(nqbits-1)])
-fullckt.draw()
+# print("### Grover's Circuit ################################")
+# fullckt.draw()
 
-print("Take from the following, best of 5 results...")
-for m in range(5):  # Look for best of 5
+maxattempts = 5
+for m in range(maxattempts):  # Look for best of all attempts
 	bk = qckt.Backend()
 	bk.run(fullckt)
-
 	res = bk.get_creg()
-	print("Result = ",res)
+	print("Result = ",res.intvalue)
+	print()
+
+	### Verify if the resultis correct
+	verifyckt = qckt.QCkt(nqbits,name="Verify")
+	x_list = []
+	for i in range(nqbits-1):
+		if (res.intvalue & (0b1<<i)) != 0:
+			x_list.append(i)
+	verifyckt.X(x_list)
+	verifyckt = verifyckt.append(uf_ckt)
+	verifyckt.M([nqbits-1],[0])
+	# print("### Verification Circuit ################################")
+	# verifyckt.draw()
+
+	bk = qckt.Backend()
+	bk.run(verifyckt)
+	creg = bk.get_creg()
+	if creg.intvalue == 1:
+		print("CORRECT Result in ",m+1,"attempts")
+		break
