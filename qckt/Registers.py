3994062c90961a64cbe06f28a27784d6796ec098
Atul Varshneya <atul.varshneya@gmail.com>
2021-04-29 19:20:51 -0700

    return register type fixed

--- qckt/Registers.py
+++ qckt/Registers.py
@@ -14,25 +14,34 @@ class Register(list):
 		for q in range(len(self)):
 			self[q] = loc - q
 		self.placed = True
+		return self
 class QRegister(Register):
 	pass
 class CRegister(Register):
 	pass
 
 def placement(*regs):
-	qbitplaced = []
-	cbitplaced = []
+	qbitplaced = QRegister(0).placeMSB(0) # trick to mark the register 'placed'
+	cbitplaced = CRegister(0).placeMSB(0) # trick to mark the register 'placed'
 	qbitsz = None
 	cbitsz = None
 	for r in reversed(regs):
 		if type(r) is QRegister:
+			if r.placed: raise QCktException("Attempt to place already placed QRegister")
 			qbitsz = qbitsz + len(r) if qbitsz is not None else len(r)
 			r.placeMSB(qbitsz - 1)
-			qbitplaced = r[:] + qbitplaced
+			temp = QRegister(0).placeMSB(0) # trick to mark the register 'placed'
+			for i in r: temp.append(i)
+			temp.extend(qbitplaced)
+			qbitplaced = temp
 		elif type(r) is CRegister:
+			if r.placed: raise QCktException("Attempt to place already placed CRegister")
 			cbitsz = cbitsz + len(r) if cbitsz is not None else len(r)
 			r.placeMSB(cbitsz - 1)
-			cbitplaced = r[:] + cbitplaced
+			temp = CRegister(0).placeMSB(0) # trick to mark the register 'placed'
+			for i in r: temp.append(i)
+			temp.extend(cbitplaced)
+			cbitplaced = temp
 		else:
 			raise QCktException("placement not supported for type:"+type(r).__name__)
 	return qbitsz,cbitsz,qbitplaced,cbitplaced
