0c3ff17db55cf5bbadd493ebb7ab20b2fc2fd118
Atul Varshneya <atul.varshneya@gmail.com>
2023-01-11 20:43:32 -0800

    added tests for runstats

---
+++ qckt/tests/runstats.py
@@ -0,0 +1,25 @@
+import qckt
+import numpy as np
+from QSystems import *
+from Job import Job
+
+ck = qckt.QCkt(4)
+ck.X(0)
+ck.X(1)
+ck.X(2)
+ck.X(3)
+ck.H(1)
+ck.H(2)
+ck.CX(1,2)
+ck.draw()
+job = Job(ck)
+Qdeb().runjob(job)
+
+print('Total steps',job.runstats['QSteps'])
+op_names = [k for k in job.runstats['OpCounts'].keys()]
+op_names.sort() # so the print order is guaranteed to be the same each time
+for k in op_names:
+	print('op',k,'count',job.runstats['OpCounts'][k])
+
+# Not using job.print_runstats(), as the actual execution time measurements will
+# be slightly different for each run, so can lead to differnt output each time.
---
+++ qsim/tests/runstats.py
@@ -0,0 +1,25 @@
+
+import qsim
+import qgates
+from qSimException import *
+
+q = qsim.QSimulator(4,qtrace=True)
+
+# H gate
+q.qgate(qgates.H(),[2])
+q.qgate(qgates.H(),[3])
+# C gate
+q.qgate(qgates.C(),[2,0])
+q.qgate(qgates.C(),[3,1])
+
+## X gate
+q.qgate(qgates.X(),[0])
+q.qgate(qgates.X(),[1])
+q.qgate(qgates.X(),[2])
+q.qgate(qgates.X(),[3])
+
+print('Total operation steps', q.qsteps)
+op_names = [k for k in q.op_counts]
+op_names.sort()
+for k in op_names:
+	print('    ',k, q.op_counts[k])
