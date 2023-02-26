615f72e0e6c416b9b688634186d7bf90a82bc2a0
Atul Varshneya <atul.varshneya@gmail.com>
2023-02-26 11:03:28 -0800

    some improvements to speed it up a bit

--- qckt/examples/add-qckt.py
+++ qckt/examples/add-qckt.py
@@ -2,18 +2,20 @@
 
 print("""
 PROBLEM STATEMENT:
-Adds 2 2-bit numbers, result is 3-bits, and scratch register is 3-bits (to hold carry overs)
-Total of 10 qubits, hence simulator runs quite slow - it took 4 minutes elapsed time on my old laptop.
-The circuit used is --
+Adds 2 2-bit numbers, result is 3-bits, and scratch register is 2-bits holdch holds carry overs for bits 1 and 2,
+there is no carry over value expected for bit 0, hence no qubit for that
 
-        ci+1	----------o---o-----        --.--
-        ci	------.---|---.-----        --|--
-	              |   |   |               |
-        bi	----.-|---.---|-----        --|--
-        ai	--.-|-|---.---|-----        --|--
-	          | | |       |               |
-        si+1	--|-|-|-------|-----        --o--
-        si	--o-o-o-----x-.-x---        -----
+Total of 9 qubits, hence simulator took just over 1 second elapsed time on my laptop.
+The circuit blueprint used is --
+
+        ci+1    ----------o---o-----        --.--
+        ci      ------.---|---.-----        --|--
+                      |   |   |               |
+        bi      ----.-|---.---|-----        --|--
+        ai      --.-|-|---.---|-----        --|--
+                  | | |       |               |
+        si+1    --|-|-|-------|-----        --o--
+        si      --o-o-o-----x-.-x---        -----
 
 Need to copy the final carry-out bit into the highest order result bit, as shown.
 
@@ -24,12 +26,12 @@ import qckt
 from QSystems import *
 from Job import Job
 
-nq = 10
+nq = 9
 
 s = [0, 1, 2]	# sum bits - Result of addition
-c = [3, 4, 5]	# carry bits, scratch pad register - Junk bits
-b = [6, 7]	# input number B
-a = [8, 9]	# input number A
+c = [3, 4]	# carry bits, scratch pad register - Junk bits
+b = [5, 6]	# input number B
+a = [7, 8]	# input number A
 
 addckt = qckt.QCkt(nq, nq)
 
@@ -43,19 +45,22 @@ for i in range(2):
 	# print("Processing bit", i)
 	addckt.CX(a[i],s[i])
 	addckt.CX(b[i],s[i])
-	addckt.CX(c[i],s[i])
-	addckt.CCX(a[i],b[i],c[i+1])
-	addckt.X(s[i])
-	addckt.CCX(s[i],c[i],c[i+1])
-	addckt.X(s[i])
-addckt.CX(c[2],s[2])
+	if i != 0:
+		addckt.CX(c[i-1],s[i])
+	addckt.CCX(a[i],b[i],c[i])
+	if i != 0:
+		addckt.X(s[i])
+		addckt.CCX(s[i],c[i-1],c[i])
+		addckt.X(s[i])
+addckt.CX(c[1],s[2])
 
 # Clean up the junk bits
 for i in reversed(range(2)):
-	addckt.X(s[i])
-	addckt.CCX(s[i],c[i],c[i+1])
-	addckt.X(s[i])
-	addckt.CCX(a[i],b[i],c[i+1])
+	if i != 0:
+		addckt.X(s[i])
+		addckt.CCX(s[i],c[i-1],c[i])
+		addckt.X(s[i])
+	addckt.CCX(a[i],b[i],c[i])
 # addckt.M([0,1,2],[0,1,2])
 
 addckt.draw()
@@ -68,3 +73,6 @@ print("READ OUT STATE VECTOR: ")
 print(job.get_svec())
 print("READ OUT CREGISTER: ", end="")
 print( job.get_creg()[0])
+print()
+print('READ OUT RUNSTATS')
+job.print_runstats()
--- qsim/examples/add.py
+++ qsim/examples/add.py
@@ -2,18 +2,20 @@
 
 print("""
 PROBLEM STATEMENT:
-Adds 2 2-bit numbers, result is 3-bits, and scratch register is 3-bits (to hold carry overs)
-Total of 10 qubits, hence simulator runs quite slow - it took 4 minutes elapsed time on my old laptop.
-The circuit used is --
+Adds 2 2-bit numbers, result is 3-bits, and scratch register is 2-bits holdch holds carry overs for bits 1 and 2,
+there is no carry over value expected for bit 0, hence no qubit for that
 
-        ci+1	----------o---o-----        --.--
-        ci	------.---|---.-----        --|--
-	              |   |   |               |
-        bi	----.-|---.---|-----        --|--
-        ai	--.-|-|---.---|-----        --|--
-	          | | |       |               |
-        si+1	--|-|-|-------|-----        --o--
-        si	--o-o-o-----x-.-x---        -----
+Total of 9 qubits, hence simulator took just over 1 second elapsed time on my laptop.
+The circuit blueprint used is --
+
+        ci+1    ----------o---o-----        --.--
+        ci      ------.---|---.-----        --|--
+                      |   |   |               |
+        bi      ----.-|---.---|-----        --|--
+        ai      --.-|-|---.---|-----        --|--
+                  | | |       |               |
+        si+1    --|-|-|-------|-----        --o--
+        si      --o-o-o-----x-.-x---        -----
 
 Need to copy the final carry-out bit into the highest order result bit, as shown.
 
@@ -24,12 +26,12 @@ import qsim
 import qgates as qgt
 import qgatesUtils as qgu
 
-q = qsim.QSimulator(10,qtrace=False)
+q = qsim.QSimulator(9,qtrace=False)
 
 s = [0, 1, 2]	# sum bits - Result of addition
-c = [3, 4, 5]	# carry bits, scratch pad register - Junk bits
-b = [6, 7]	# input number B
-a = [8, 9]	# input number A
+c = [3, 4]	# carry bits, scratch pad register - Junk bits
+b = [5, 6]	# input number B
+a = [7, 8]	# input number A
 
 H4 = qgu.qcombine_par("H4",[qgt.H(),qgt.H(),qgt.H(),qgt.H()])
 q.qgate(H4,[a[0],a[1],b[0],b[1]])
@@ -39,19 +41,22 @@ for i in range(2):
 	print("Processing bit", i)
 	q.qgate(qgt.C(),[a[i],s[i]])
 	q.qgate(qgt.C(),[b[i],s[i]])
-	q.qgate(qgt.C(),[c[i],s[i]])
-	q.qgate(qgt.T(),[a[i],b[i],c[i+1]])
-	q.qgate(qgt.X(),[s[i]])
-	q.qgate(qgt.T(),[s[i],c[i],c[i+1]])
-	q.qgate(qgt.X(),[s[i]])
-q.qgate(qgt.C(),[c[2],s[2]])
+	if i != 0:
+		q.qgate(qgt.C(),[c[i-1],s[i]])
+	q.qgate(qgt.T(),[a[i],b[i],c[i]])
+	if i != 0:
+		q.qgate(qgt.X(),[s[i]])
+		q.qgate(qgt.T(),[s[i],c[i-1],c[i]])
+		q.qgate(qgt.X(),[s[i]])
+q.qgate(qgt.C(),[c[1],s[2]])
 
 # Clean up the junk bits
 print("Cleaning up junk bits")
 for i in reversed(range(2)):
-	q.qgate(qgt.X(),[s[i]])
-	q.qgate(qgt.T(),[s[i],c[i],c[i+1]])
-	q.qgate(qgt.X(),[s[i]])
-	q.qgate(qgt.T(),[a[i],b[i],c[i+1]])
+	if i != 0:
+		q.qgate(qgt.X(),[s[i]])
+		q.qgate(qgt.T(),[s[i],c[i-1],c[i]])
+		q.qgate(qgt.X(),[s[i]])
+	q.qgate(qgt.T(),[a[i],b[i],c[i]])
 
 q.qreport("Result")
