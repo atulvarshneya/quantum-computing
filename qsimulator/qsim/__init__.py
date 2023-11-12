4be8eb251085ec8252ffbdc623622c2d1715276c
Atul Varshneya <atul.varshneya@gmail.com>
2023-11-11 22:40:25 -0800

    included Qsim util functions

--- qsimulator/qsim/__init__.py
+++ qsimulator/qsim/__init__.py
@@ -1,7 +1,6 @@
 
 from qsim.qSimException import QSimError
-from qsim.Qsim import QSimulator
+from qsim.Qsim import QSimulator, qstretch, qinverse, qisunitary
 from qsim.NISQsim import NISQSimulator
 from qsim.qgates import X, Y, Z, H, Rphi, Rk, SQSWAP, SWAP, CSWAP, QFT, Hn, RND, CTL, C, T, BELL_BASIS, HDM_BASIS
 from qsim.qgatesUtils import qcombine_seq, qcombine_par
-
