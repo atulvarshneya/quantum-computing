1b762b9bde825f16b810073d322a1ed0fa3c2f9b
Atul Varshneya <atul.varshneya@gmail.com>
2023-11-15 22:34:08 -0800

    added __version__, __author__, __email__

--- qcircuit/qckt/__init__.py
+++ qcircuit/qckt/__init__.py
@@ -4,3 +4,7 @@ from qckt.Registers import QRegister, CRegister, placement
 from qckt.Job import Job
 from qckt.qException import QCktException
 
+__version__ = '1.5'
+__author__ = 'Atul Varshneya'
+__email__ = 'atul.varshneya@gmail.com'
+
--- qsimulator/qsim/__init__.py
+++ qsimulator/qsim/__init__.py
@@ -4,3 +4,8 @@ from qsim.Qsim import QSimulator
 from qsim.NISQsim import NISQSimulator
 from qsim.qgates import X, Y, Z, H, Rphi, Rk, SQSWAP, SWAP, CSWAP, QFT, Hn, RND, CTL, C, T, BELL_BASIS, HDM_BASIS
 from qsim.qgatesUtils import qcombine_seq, qcombine_par
+
+__version__ = '1.5'
+__author__ = 'Atul Varshneya'
+__email__ = 'atul.varshneya@gmail.com'
+
