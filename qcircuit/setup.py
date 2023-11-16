4c44127476ac1e4964eeeef236e2af663f2a42c4
Atul Varshneya <atul.varshneya@gmail.com>
2023-11-16 01:12:02 -0800

    ready for upload to pypi

--- qcircuit/setup.py
+++ qcircuit/setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
 setup(
-    name='qckt',
+    name='qcircuit',
     version='1.5',
     license='GPL',
     author='Atul Varshneya',
     author_email='atul.varshneya@gmail.com',
+	description='Quantum computer programming with circuits paradigm',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
     packages=find_packages('.'),
     # package_dir={'': '.'},
     keywords='qsystems quantum computing',
     install_requires=[
         'numpy',
-        'qsim>=1.5'
+        'qsimulator>=1.5'
         ]
     )
--- qsimulator/setup.py
+++ qsimulator/setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
 setup(
-    name='qsim',
+    name='qsimulator',
     version='1.5',
     license='GPL',
     author='Atul Varshneya',
     author_email='atul.varshneya@gmail.com',
+	description='Quantum computer simulator',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
     packages=find_packages('.'),
     # package_dir={'': '.'},
     keywords='qsystems quantum computing',
