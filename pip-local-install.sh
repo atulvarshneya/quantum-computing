6838933a6bc0d90ed7e0a5f074ceefb489b26957
Atul Varshneya <atul.varshneya@gmail.com>
2023-12-10 07:21:30 -0800

    pip installation with 'editable' option (-e)

--- pip-local-install.sh
+++ pip-local-install.sh
@@ -4,9 +4,9 @@ select=${1:-all}
 
 if [ $select == "all" ] || [ $select == "qusimulator" ]; then
 	pushd qusimulator
-	pip install .
+	pip install -e .
 	rm -r build
-	rm -r qusimulator.egg-info
+	# rm -r qusimulator.egg-info
 	rm -r __pycache__
 	popd
 	echo '-----------------------------------------------------'
@@ -14,9 +14,9 @@ fi
 
 if [ $select == "all" ] || [ $select == "qucircuit" ]; then
 	pushd qucircuit
-	pip install .
+	pip install -e .
 	rm -r build
-	rm -r qucircuit.egg-info
+	# rm -r qucircuit.egg-info
 	rm -r __pycache__
 	popd
 	echo '-----------------------------------------------------'
