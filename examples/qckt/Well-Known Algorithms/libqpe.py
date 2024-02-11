6b4de4b10c236f237bad8e86bb20d102f5c94b4e
Atul Varshneya <atul.varshneya@gmail.com>
2024-02-11 14:01:45 -0800

    aligned with Release 2.2

--- examples/qckt/Well-Known Algorithms/libqpe.py
+++ examples/qckt/Well-Known Algorithms/libqpe.py
@@ -8,16 +8,25 @@ class QPE:
 	def __init__(self, target_uop, uop_qubits, measurement_qubits):
 		self.qpe_circuit = None
 
+		# create a user defined gate for controlled-uop
+		ctrl_func_op = gutils.CTL(target_uop)
+		qckt.define_gate("CUOP", ctrl_func_op)
+
+		# also create inverse quantum Fourier transform gate
+		nqft = len(measurement_qubits)
+		qftckt_qubits = [i for i in reversed(range(nqft))]
+		ckt = qckt.QCkt(nqft)
+		ckt.QFT(*(qftckt_qubits))
+		mat = ckt.to_opMatrix()
+		QFTinvOp = gutils.opmat_dagger(mat)
+		qckt.define_gate("QFTinv", QFTinvOp)
+
 		# start the QPE circuit
 		nqubits = len(measurement_qubits + uop_qubits)
 		qc = qckt.QCkt(nqubits)
 
 		# put all measuremnt qubits in full superposition
 		qc.H(measurement_qubits)
-
-		# create a custom gate for controlled-uop
-		ctrl_func_op = gutils.CTL(target_uop)
-		qc.custom_gate("CUOP", ctrl_func_op)
 
 		# Apply the controlled unitary operators in sequence
 		len_measq = len(measurement_qubits)
@@ -26,15 +35,6 @@ class QPE:
 			for r in range(uop_exponent):
 				qc.CUOP(*([measurement_qubits[len_measq-ctrl_qubit-1]] + uop_qubits))
 			uop_exponent *= 2
-
-		# to apply the inverse quantum Fourier transform, first build the inverse-QFT gate
-		nqft = len(measurement_qubits)
-		qftckt_qubits = [i for i in reversed(range(nqft))]
-		ckt = qckt.QCkt(nqft)
-		ckt.QFT(*(qftckt_qubits))
-		mat = ckt.to_opMatrix()
-		QFTinvOp = gutils.opmat_dagger(mat)
-		qc.custom_gate("QFTinv", QFTinvOp)
 
 		# complete the circuit with inverse-QFT on measurement_qubits
 		qc.QFTinv(*measurement_qubits)
